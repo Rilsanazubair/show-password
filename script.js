2a5ac774ad4f07f88bb4109abab2b073f7cb2ae8
Rilsanazubair <rilzanazubai@gmail.com>
2023-09-27 13:57:53 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let eyeIcon = document.getElementById("eyeicon");
+let passWord = document.getElementById("password");
+
+eyeIcon.onclick = function() {
+    if(passWord.type == "password")
+    {
+        passWord.type = "text";
+        eyeIcon.src = "eye-open.png";
+    }
+    else{
+        passWord.type = "password";
+        eyeIcon.src = "eye-close.png";
+    }
+}
